e08535a5b8aa3a4e7b2f916081f24621cd6de27c
brunooctaviodesouzaoliveira2-cpu <brunooctaviodesouzaoliveira2@gmail.com>
2026-06-15 14:10:07 -0300

    1506

---
+++ meu_js.js
@@ -0,0 +1,10 @@
+        //string
+        var texto = 'curso de JavaScript';
+        //number
+        var numberInteiro = -7;
+        var numberFracionado = 10.45;
+        //boolean
+        var teste = true; //true-false
+         alert(texto)
+        document, write(texto)
+        document, log(texto)
